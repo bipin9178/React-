0c8a3817b15a4ddadc1ec590a6dd5f6ea93797ba
bipin9178 <bipinpkanzariya@gmail.com>
2025-08-19 11:23:48 +0530

    react element and  jsx function

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -3,11 +3,12 @@ import Chai from "./chai"
 
 function App() {
 
+    const username = "chai aur code  "
 
   return (
   <>
     <Chai />
-    <h1>chai aur rwact </h1>
+    <h1>chai aur react  {username}</h1>
       <p>test para</p>
     </>
   )
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Cahi</h1>
+    </div>
+  )
+}
+
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://www.google.com',
+//     target: '_blank',
+    
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+
+ <a href="https://google.com" target='_blank'>visit google</a>
+)
+const anotheruser = "chai aur react"
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://www.google.com', target: '_blank' },
+  'Click Me To Visit Google',
+  anotheruser
+)
+createRoot(document.getElementById('root')).
+  render(
+
+  reactElement
 )
